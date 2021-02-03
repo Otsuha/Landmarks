ed9069e47705df5c03276af5c9da2b8fbfb0fd01
Plainson <PlainsonLGrass@outlook.com>
2021-02-03 19:26:53 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Grass Plainson on 2021/2/3.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
